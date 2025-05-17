cca6976646ff3efe6bdda7cb3f51b43031760df1
dvrishabh02 <devrishabhsharma02@gmail.com>
2025-05-17 16:27:52 +0530

    Added Material UI components

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,26 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import PropertyList from './PropertyList'
+import Button from '@mui/material/Button';
 
-const properties=[
+
+function App() {
+  const properties=[
   { id: 1, name: 'Property 1', price: 100000 },
   { id: 2, name: 'Property 2', price: 200000 },
   { id: 3, name: 'Property 3', price: 300000 },
 ];
-function App() {
+    // const print=()=>{
+    //   properties.forEach((property) => {
+    //     property.name="Good Property ";
+    //   });
+    // }
 
 
   return (
     <>
       <PropertyList properties={properties} />
+      <Button variant="contained">Press Me</Button>
     </>
   )
 }
